eb70bcdabbf92c18c781508ee85f8904b4af758b
Mario Amaya <thefozcat@gmail.com>
2019-05-15 14:38:14 -0700

    added toggleClosed test and toggleLocked test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,12 +1,41 @@
 // Test away
 import React from 'react';
 import Dashboard from '../dashboard/Dashboard';
+import Controls from '../controls/Controls';
 
 import 'react-testing-library/cleanup-after-each';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 
 describe('<Dashboard/>', () => {
     it('renders dashboard', () => {
         render(<Dashboard/>)
+    })
+
+    describe('toggleClosed()', () => {
+        it('should close the gate', () => {
+            const spy = jest.fn();
+            const { getByText } = render(<Controls toggleClosed={spy}/>)
+            const button = getByText(/close gate/i)
+
+            fireEvent.click(button);
+
+            expect(spy).toHaveBeenCalledTimes(1);
+        })
+    })
+
+    describe('toggleLocked()', () => {
+        it('should lock the gate', () => {
+            const spy = jest.fn();
+
+            const { getByText } = render(<Controls toggleLocked={spy}/>)
+            const closeBtn = getByText(/close gate/i)
+
+            fireEvent.click(closeBtn);
+    
+            const lockBtn = getByText(/lock gate/i);
+            fireEvent.click(lockBtn);
+            spy();
+            expect(spy).toHaveBeenCalledTimes(1);
+        })
     })
 })
